cdd0e575bd19675c6a14a951a5b1350791fc2f5c
simengyuan <2441241367@qq.com>
2021-06-08 20:37:27 +0800

    修改数据库

--- blogserver/src/main/java/org/sang/BlogserverApplication.java
+++ blogserver/src/main/java/org/sang/BlogserverApplication.java
@@ -2,7 +2,10 @@ package org.sang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 @SpringBootApplication
 @EnableScheduling//开启定时任务支持
@@ -11,4 +14,11 @@ public class BlogserverApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlogserverApplication.class, args);
     }
+    @Bean
+    public HttpFirewall allowUrlSemicolonHttpFirewall() {
+        StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowSemicolon(true);
+        return firewall;
+    }
+
 }
