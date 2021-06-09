57c8c61810f444c440bd45eb96b895292256055e
simengyuan <2441241367@qq.com>
2021-06-09 14:43:10 +0800

    修改 war配置

--- blogserver/src/main/java/org/sang/BlogserverApplication.java
+++ blogserver/src/main/java/org/sang/BlogserverApplication.java
@@ -2,6 +2,8 @@ package org.sang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.web.firewall.HttpFirewall;
@@ -9,10 +11,14 @@ import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 @SpringBootApplication
 @EnableScheduling//开启定时任务支持
-public class BlogserverApplication {
+public class BlogserverApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogserverApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BlogserverApplication.class);
     }
     @Bean
     public HttpFirewall allowUrlSemicolonHttpFirewall() {
